49d7b86b443d1f6f0ad11d5c3d5d3d3b4bc154de
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-04-07 15:34:11 +0700

    menambahkan integer i

--- StructArray.cpp
+++ StructArray.cpp
@@ -10,3 +10,22 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    //Deklarasi obyek dari struct orang
+    orang mhs[3];
+    //Mengisi Obyek
+    for (int i = 0; i <= 2; i++){
+        cout << "Nama Mahasiswa:";
+    getline(cin, mhs[i].nama);
+    cout << "Alamat Desa:";
+    getline(cin, mhs[i].alamat.desa);
+    cout << "Alamat Kora:";
+    getline(cin, mhs[i].alamat.kota);
+    cout << "Umur:";
+    cin >> mhs[i].umur;
+    cout << endl;
+    }
+    cout << endl;
+}
